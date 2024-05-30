4ad5f7e0e9f4a723966db19ccd6eacffcfe0c0d6
Mark McTague <mmcta275@gmail.com>
2024-05-30 17:31:36 -0400

    Poolboys pool models in and out created

---
+++ api/models/pools.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+
+class PoolIn(BaseModel):
+    """
+    Creates a pool.
+    """
+
+    picture_url: str
+    address: str
+    days_available: str
+    description: str
+    hourly_rate: int
+    number_guests: int
+    poolowner_id: int
+
+class PoolOut(BaseModel):
+    """
+    Get a pool
+    """
+    id: int
+    picture_url: str
+    address: str
+    days_available: str
+    description: str
+    hourly_rate: int
+    number_guests: int
+    poolowner_id: int
