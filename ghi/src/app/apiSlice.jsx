aa35c42a7a6e34b99cf7a821f202a38fa1ec55a8
Hasan <hamdanhm1986@gmail.com>
2024-06-11 18:58:46 -0400

    Get pool by id not flushed

--- ghi/src/app/apiSlice.jsx
+++ ghi/src/app/apiSlice.jsx
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const PoolBoysApi = createApi({
     reducerPath: 'PoolBoysApi',
@@ -12,58 +12,73 @@ export const PoolBoysApi = createApi({
         }),
         getUser: builder.query({
             query: () => ({
-                url: '/api/auth/authenticate'
+                url: '/api/auth/authenticate',
             }),
-            providesTags: ['User']
+            providesTags: ['User'],
         }),
         signoutUser: builder.mutation({
             query: () => ({
                 url: '/api/auth/signout',
-                method: 'DELETE'
+                method: 'DELETE',
             }),
-            invalidatesTags: ['User']
+            invalidatesTags: ['User'],
         }),
         signinUser: builder.mutation({
             query: (body) => ({
                 url: 'api/auth/signin',
                 method: 'POST',
-                body
+                body,
             }),
-            invalidatesTags: ['User']
+            invalidatesTags: ['User'],
         }),
         signupUser: builder.mutation({
             query: (body) => ({
                 url: 'api/auth/signup',
                 method: 'POST',
-                body
+                body,
             }),
-            providesTags: ['User']
+            providesTags: ['User'],
         }),
         createPool: builder.mutation({
             query: (body) => ({
                 url: 'api/pools',
                 method: 'POST',
-                body
+                body,
             }),
-            providesTags: ['Pool']
+            providesTags: ['Pool'],
         }),
         getPoolDetails: builder.query({
             query: (id) => ({
-                url: `api/pools/${id}`
+                url: `api/pools/${id}`,
             }),
-            providesTags: ['Pool']
+            providesTags: ['Pool'],
         }),
         deletePool: builder.mutation({
             query: () => ({
                 url: `/api/pools/${id}`,
-                method: 'DELETE'
+                method: 'DELETE',
             }),
-            invalidatesTags: ['Pool']
+            invalidatesTags: ['Pool'],
+        }),
+        getReservations: builder.query({
+            query: () => ({
+                url: '/api/reservations',
+            }),
+        }),
+        createReservations: builder.mutation({
+            query: (body) => ({
+                url: '/api/reservations',
+                method: 'POST',
+                body,
+            }),
+            providesTags: ['Reservations'],
         }),
     }),
-});
+})
 
 export const {
+    useCreateReservationsMutation,
+    useGetReservationsQuery,
     useGetAllPoolsQuery,
     useGetUserQuery,
     useSignoutUserMutation,
@@ -72,132 +87,4 @@ export const {
     useCreatePoolMutation,
     useGetPoolDetailsQuery,
     useDeletePoolMutation,
-} = PoolBoysApi;
-
-
-
-
-
-
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-// export const PoolBoysApi = createApi({
-//     reducerPath: 'api',
-//     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
-//     tagTypes: ['Reservation', 'Pool', 'Amenity', 'User'],
-//     endpoints: (builder) => ({
-
-//         signin: builder.mutation({
-//             query: (credentials) => ({
-//                 url: '/signin',
-//                 method: 'POST',
-//                 body: credentials,
-//             }),
-//         }),
-        // signupUser: builder.mutation({
-        //     query: (userDetails) => ({
-        //         url: '/signup',
-        //         method: 'POST',
-        //         body: userDetails,
-        //     }),
-        //     invalidatesTags: ['User'],
-//         }),
-//         authenticateUser: builder.query({
-//             query: () => '/authenticate',
-//             providesTags: ['User'],
-//         }),
-//         signoutUser: builder.mutation({
-//             query: () => ({
-//                 url: '/signout',
-//                 method: 'DELETE',
-//             }),
-//             invalidatesTags: ['User'],
-//         }),
-
-
-//         createReservation: builder.mutation({
-//             query: (newReservation) => ({
-//                 url: '/api/reservations',
-//                 method: 'POST',
-//                 body: newReservation,
-//             }),
-//             invalidatesTags: ['Reservation'],
-//         }),
-//         getAllReservations: builder.query({
-//             query: () => '/api/reservations',
-//             providesTags: ['Reservation'],
-//         }),
-//         getReservationById: builder.query({
-//             query: (id) => `/api/reservations/${id}`,
-//             providesTags: ['Reservation'],
-//         }),
-//         deleteReservation: builder.mutation({
-//             query: (id) => ({
-//                 url: `/api/reservations/${id}`,
-//                 method: 'DELETE',
-//             }),
-//             invalidatesTags: ['Reservation'],
-//         }),
-
-
-//         createPool: builder.mutation({
-//             query: (newPool) => ({
-//                 url: '/api/pools',
-//                 method: 'POST',
-//                 body: newPool,
-//             }),
-//             invalidatesTags: ['Pool'],
-//         }),
-//         getAllPools: builder.query({
-//             query: () => '/api/pools',
-//             providesTags: ['Pool'],
-//         }),
-//         getPoolDetails: builder.query({
-//             query: (poolId) => `/api/pools/${poolId}`,
-//             providesTags: ['Pool'],
-//         }),
-//         updatePool: builder.mutation({
-//             query: ({ poolId, data }) => ({
-//                 url: `/api/pools/${poolId}`,
-//                 method: 'PUT',
-//                 body: data,
-//             }),
-//             invalidatesTags: ['Pool'],
-//         }),
-//         deletePool: builder.mutation({
-//             query: (poolId) => ({
-//                 url: `/api/pools/${poolId}`,
-//                 method: 'DELETE',
-//             }),
-//             invalidatesTags: ['Pool'],
-//         }),
-
-
-//         getAllAmenities: builder.query({
-//             query: () => '/api/amenities',
-//             providesTags: ['Amenity'],
-//         }),
-//         getAmenityById: builder.query({
-//             query: (id) => `/api/amenities/${id}`,
-//             providesTags: ['Amenity'],
-//         }),
-//     }),
-// });
-
-// export const {
-//     useSigninMutation,
-//     useSignupUserMutation,
-//     useAuthenticateUserQuery,
-//     useSignoutUserMutation,
-//     useCreateReservationMutation,
-//     useGetAllReservationsQuery,
-//     useGetReservationByIdQuery,
-//     useDeleteReservationMutation,
-//     useCreatePoolMutation,
-//     useGetAllPoolsQuery,
-//     useGetPoolDetailsQuery,
-//     useUpdatePoolMutation,
-//     useDeletePoolMutation,
-//     useGetAllAmenitiesQuery,
-//     useGetAmenityByIdQuery,
-// } = PoolBoysApi;
+} = PoolBoysApi
--- ghi/src/components/Home.jsx
+++ ghi/src/components/Home.jsx
@@ -1,13 +1,25 @@
 import { useGetUserQuery } from "../app/apiSlice";
 import { useSignoutUserMutation } from "../app/apiSlice";
+import { useCreateReservationsMutation } from '../app/apiSlice'
 
 
 
 
-const Home = () => {
-    return (
-                <h1>Hello</h1>
-    );
-};
+    const Home = () => {
+        const {
+            data: reservations,
+            isLoading,
+            error,
+        } = useCreateReservationsMutation()
+        console.log(reservations)
+        if (isLoading) return <div>Loading pools...</div>
+        if (error) return <div>Error loading pools: {error.message}</div>
+        return (<h1>Hello</h1>)
+    }
+
+
+
+
+
 
 export default Home;
