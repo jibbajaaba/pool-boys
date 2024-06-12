acee9d364d5781044425615272358cf88414c9fa
Mark McTague <mmcta275@gmail.com>
2024-06-12 15:31:55 -0400

    fix home

--- ghi/src/components/Home.jsx
+++ ghi/src/components/Home.jsx
@@ -1,25 +1,9 @@
-import { useGetUserQuery } from "../app/apiSlice";
-import { useSignoutUserMutation } from "../app/apiSlice";
-import { useCreateReservationsMutation } from '../app/apiSlice'
 
 
-
-
-    const Home = () => {
-        const {
-            data: reservations,
-            isLoading,
-            error,
-        } = useCreateReservationsMutation()
-        console.log(reservations)
-        if (isLoading) return <div>Loading pools...</div>
-        if (error) return <div>Error loading pools: {error.message}</div>
-        return (<h1>Hello</h1>)
-    }
-
-
-
-
-
+const Home = () => {
+    return (
+        <h1>Hi!</h1>
+    )
+}
 
 export default Home;
