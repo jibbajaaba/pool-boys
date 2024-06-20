93b9fc9cb84805971ba93cb46c1d0fea0f126a54
cosimo <cosimoblodgett9@gmail.com>
2024-06-20 10:41:50 -0400

    CarouselFixed

--- ghi/src/components/Home.jsx
+++ ghi/src/components/Home.jsx
@@ -3,6 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../App.css';
+import '../carousel.css';
 
 const Home = () => {
     const settings = {
